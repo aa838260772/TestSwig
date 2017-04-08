6f91ca4e86616517b39e4bea1c596619dae9905d
aa838260772 <838260772@qq.com>
2017-04-08 23:41:57 +0800

    add login logic

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var mongooseModel = require('../modules/mongoseOpt')
+var dbo = require('../modules/mongoseOpt').mongoose;
+var Schema = dbo.Schema;
+
+
+var userSchema = Schema({userid:String, password:String});
+var Users = dbo.model('users', userSchema, 'users');
 //登录接口
 router.post('/', function(req, res, next){
 		//    //用户名、密码、验证码
@@ -8,16 +13,24 @@ router.post('/', function(req, res, next){
 		var password = req.body.password;
 
 		var findStr = {userid:username};
-		mongooseModel.findOne(findStr,function(error, result){
+		Users.findOne(findStr,function(error, result){
 			if(error) {
 				return res.send({status:0,info:'登录失败'});
 			}
-			console.log(result);
-			if(result.userid){
-				return res.send({status:1,info:'登录成功'});
+			//console.log(result);
+			if(!result){
+				console.log('用户名不存在!');
+				return res.send({status:0,info:'用户不存在'});
 			}
 			else{
-				return res.send({status:0,info:'用户不存在'});
+				console.log('用户名存在!');
+				if(!result.password || result.password !== password){
+
+					console.log('密码错误！');
+					return res.send({status:1,info:'密码错误'});
+				}
+				console.log('密码验证成功');
+				return res.send({status:1,info:'登录成功'});
 			}
 			});
 		});
